696fca35f17ecdc950ad83dd272c4b0353d3bcfa
Mohammed Abu Waked <mhwk859@gmail.com>
2026-04-15 16:33:59 +0300

    movie

--- main.js
+++ main.js
@@ -44,6 +44,7 @@ fetch("https://api.imdbapi.dev/titles")
     return result.json();
   })
   .then((data) => {
+    console.log(data);
     for (let i = 0; i < 20; i++) {
       let box = document.createElement("div");
       box.className = "box";
@@ -54,27 +55,33 @@ fetch("https://api.imdbapi.dev/titles")
       document.querySelector("#movies").appendChild(value);
       box.appendChild(img);
       document.querySelector(".boxs").appendChild(box);
-      img.style.width = "120px";
-      img.style.height = "200px";
-      img.style.borderRadius = "9px";
-      img.style.opacity = "0.5";
+      img.style.opacity = "0.3";
       img.style.transition = " 0.3s";
-      box.style.backgroundColor = "black";
-      box.style.borderRadius = "9px";
       box.style.cursor = "pointer";
       if (i === 0) {
+        img.style.margin = "0 12px";
         loading.style.display = "none";
         img.classList.add("active");
         img.style.opacity = "1";
-        img.style.transform = "scaleX(1.2) scaleY(1.15)";
+        if (window.matchMedia("(max-width: 768px)").matches) {
+          img.style.transform = "scaleX(1.18) scaleY(1.189)";
+        } else {
+          img.style.transform = "scaleX(1.18) scaleY(1.189)";
+        }
         img.style.transformOrigin = "bottom center";
         document.querySelector(".left").style.background = `
-  linear-gradient(to right, rgba(0,0,0,0.53), rgba(0,0,0,0)),
+  linear-gradient(to right, rgba(0,0,0,0.53) 10% , transparent 100%),
   url(${data.titles[0].primaryImage.url}) center / cover no-repeat
 `;
-        document.querySelector(".right").style.background = `
-  linear-gradient(to left, rgba(0,0,0,0.9) 60%, rgba(0,0,0,0.7) 90%, rgba(0,0,0,0.53) 100%),
+        if (window.matchMedia("(max-width: 768px)").matches) {
+          document.querySelector(".right").style.background = `
+  linear-gradient(to left, rgba(0,0,0,0.9) 10%, rgba(0,0,0,0.7) 40%, rgba(0,0,0,0.53) 60%),
   url(${data.titles[0].primaryImage.url}) center / cover no-repeat `;
+        } else {
+          document.querySelector(".right").style.background = `
+  linear-gradient(to left, rgba(0,0,0,0.9) 80%, rgba(0,0,0,0.7) 90%, rgba(0,0,0,0.53) 100%),
+  url(${data.titles[0].primaryImage.url}) center / cover no-repeat `;
+        }
 
         name.textContent = data.titles[i].originalTitle;
         rating.textContent = data.titles[i].rating.aggregateRating;
@@ -86,25 +93,30 @@ fetch("https://api.imdbapi.dev/titles")
           Math.floor((data.titles[i].runtimeSeconds % 3600) / 60) +
           " minutes";
         let gen = data.titles[i].genres;
-        for (let j = 0; j < gen.length; j++) {
-          type.textContent += " | " + gen[j];
-        }
+        type.textContent += " | " + gen[0];
         text.textContent = data.titles[i].plot;
         cont.style.display = "block";
         active = img;
       }
       img.addEventListener("click", function () {
         if (active) {
+          active.style.margin = "0";
           active.classList.remove("active");
-          active.style.opacity = "0.5";
+          active.style.opacity = "0.7";
           active.style.transform = "scale(1)";
           active.parentElement.style.margin = "0px";
           cont.style.display = "none";
         }
         img.classList.add("active");
         img.style.opacity = "1";
-        img.style.transform = "scaleX(1.2) scaleY(1.15)";
+        if (window.matchMedia("(max-width: 768px)").matches) {
+          img.style.transform = "scaleX(1.18) scaleY(1.189)";
+        } else {
+          img.style.transform = "scaleX(1.18) scaleY(1.189)";
+        }
+
         img.style.transformOrigin = "bottom center";
+        img.style.margin = "0 12px";
         img.parentElement.style.margin = "0px 10px0";
         cont.style.display = "block";
         name.textContent = data.titles[i].originalTitle;
@@ -118,29 +130,47 @@ fetch("https://api.imdbapi.dev/titles")
           " minutes";
         let gen = data.titles[i].genres;
         type.textContent = "";
-        for (let j = 0; j < gen.length; j++) {
-          type.textContent += " | " + gen[j];
-        }
+
+        type.textContent += " | " + gen[0];
         text.textContent = data.titles[i].plot;
         clicked = i;
         document.querySelector(".left").style.background = `
   linear-gradient(to right, rgba(0,0,0,0.53), rgba(0,0,0,0)),
   url(${data.titles[i].primaryImage.url}) center / cover no-repeat  `;
-        document.querySelector(".right").style.background = `
-  linear-gradient(to left, rgba(0,0,0,0.9) 60%, rgba(0,0,0,0.7) 90%, rgba(0,0,0,0.53) 100%),
+        if (window.matchMedia("(max-width: 768px)").matches) {
+          document.querySelector(".right").style.background = `
+  linear-gradient(to left, rgba(0,0,0,0.9) 10%, rgba(0,0,0,0.7) 40%, rgba(0,0,0,0.53) 60%),
   url(${data.titles[i].primaryImage.url}) center / cover no-repeat `;
+        } else {
+          document.querySelector(".right").style.background = `
+  linear-gradient(to left, rgba(0,0,0,0.9) 80%, rgba(0,0,0,0.7) 90%, rgba(0,0,0,0.53) 100%),
+  url(${data.titles[i].primaryImage.url}) center / cover no-repeat `;
+        }
+
         active = img;
       });
       img.addEventListener("mouseover", function () {
         img.style.opacity = "1";
-        img.style.transform = "scaleX(1.2) scaleY(1.15)";
+        img.style.margin = "0 12px";
+        if (window.matchMedia("(max-width: 768px)").matches) {
+          img.style.transform = "scaleX(1.18) scaleY(1.189)";
+        } else {
+          img.style.transform = "scaleX(1.18) scaleY(1.189)";
+        }
         img.style.transformOrigin = "bottom center";
         document.querySelector(".left").style.background = `
   linear-gradient(to right, rgba(0,0,0,0.53), rgba(0,0,0,0)),
   url(${data.titles[i].primaryImage.url}) center / cover no-repeat  `;
-        document.querySelector(".right").style.background = `
-  linear-gradient(to left, rgba(0,0,0,0.9) 60%, rgba(0,0,0,0.7) 90%, rgba(0,0,0,0.53) 100%),
+        if (window.matchMedia("(max-width: 768px)").matches) {
+          document.querySelector(".right").style.background = `
+  linear-gradient(to left, rgba(0,0,0,0.9) 10%, rgba(0,0,0,0.7) 40%, rgba(0,0,0,0.53) 60%),
   url(${data.titles[i].primaryImage.url}) center / cover no-repeat `;
+        } else {
+          document.querySelector(".right").style.background = `
+  linear-gradient(to left, rgba(0,0,0,0.9) 80%, rgba(0,0,0,0.7) 90%, rgba(0,0,0,0.53) 100%),
+  url(${data.titles[i].primaryImage.url}) center / cover no-repeat `;
+        }
+
         name.textContent = data.titles[i].originalTitle;
         rating.textContent = data.titles[i].rating.aggregateRating;
         year.textContent = " " + data.titles[i].startYear + " year ";
@@ -152,22 +182,29 @@ fetch("https://api.imdbapi.dev/titles")
           " minutes";
         let gen = data.titles[i].genres;
         type.textContent = "";
-        for (let j = 0; j < gen.length; j++) {
-          type.textContent += " | " + gen[j];
-        }
+
+        type.textContent += " | " + gen[0];
         text.textContent = data.titles[i].plot;
         cont.style.display = "block";
       });
       img.addEventListener("mouseout", function () {
         if (img !== active) {
           img.style.opacity = "0.5";
+          img.style.margin = "0 ";
           img.style.transform = "scale(1)";
           document.querySelector(".left").style.background = `
   linear-gradient(to right, rgba(0,0,0,0.53), rgba(0,0,0,0)),
   url(${data.titles[clicked].primaryImage.url}) center / cover no-repeat  `;
-          document.querySelector(".right").style.background = `
-  linear-gradient(to left, rgba(0,0,0,0.9) 60%, rgba(0,0,0,0.7) 90%, rgba(0,0,0,0.53) 100%),
+          if (window.matchMedia("(max-width: 768px)").matches) {
+            document.querySelector(".right").style.background = `
+  linear-gradient(to left, rgba(0,0,0,0.9) 10%, rgba(0,0,0,0.7) 40%, rgba(0,0,0,0.53) 60%),
   url(${data.titles[clicked].primaryImage.url}) center / cover no-repeat `;
+          } else {
+            document.querySelector(".right").style.background = `
+  linear-gradient(to left, rgba(0,0,0,0.9) 80%, rgba(0,0,0,0.7) 90%, rgba(0,0,0,0.53) 100%),
+  url(${data.titles[clicked].primaryImage.url}) center / cover no-repeat `;
+          }
+
           name.textContent = data.titles[clicked].originalTitle;
           rating.textContent = data.titles[clicked].rating.aggregateRating;
           year.textContent = " " + data.titles[clicked].startYear + " year ";
@@ -179,9 +216,8 @@ fetch("https://api.imdbapi.dev/titles")
             " minutes";
           let gen = data.titles[clicked].genres;
           type.textContent = "";
-          for (let j = 0; j < gen.length; j++) {
-            type.textContent += " | " + gen[j];
-          }
+
+          type.textContent += " | " + gen[0];
           text.textContent = data.titles[clicked].plot;
           cont.style.display = "block";
         }
